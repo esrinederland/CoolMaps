5bcd4bddf28831e59a92a3e62adce01a8c813db6
MaartjeHoltslag <mcsholtslag@gmail.com>
2022-09-16 09:42:40 +0200

    Added EmbeddedMapWithRoute

---
+++ EmbeddedMapWithRoute/Readme.md
@@ -0,0 +1,14 @@
+# Embedded Map with Route
+
+This sample shows how to display a simple map with a precalculated route on your website. The route is created using the ArcGIS MapViewer and saved as a new RouteLayer. This layer can be accessed through the Portal Item to extract the route's geometry and directions. 
+<br>
+<br>
+Use the ArcGIS API for JavaScript to embed a map and your route information on your own website. The ID of the portalItem in <i>arcgis-map.js</i> can be changed to display information from your own RouteLayer. 
+
+![Embedded Map with Route](../images/20220916_EmbeddedMapWithRoute.png)
+
+<br>
+<br>
+
+View this example live:
+[here](https://esrinederland.github.io/CoolMaps/EmbeddedMapWithRoute/index.html)
---
+++ EmbeddedMapWithRoute/js/arcgis-map.js
@@ -0,0 +1,47 @@
+require(["esri/config", "esri/Map", "esri/views/MapView", "esri/widgets/Fullscreen", "esri/layers/RouteLayer"], (Map, MapView, Fullscreen, RouteLayer) => {
+
+    // Get the route layer
+    const routeLayer = new RouteLayer({
+        portalItem: {
+            id: "98b6935b28d544c69ec12c937780f08c"
+        }
+    });
+
+    // Create an empty map
+    const map = new Map({
+        basemap: "topo-vector"
+    });
+
+    // Add the map to the home page
+    const view = new MapView({
+        container: "viewDiv",
+        map: map
+    });
+
+    // Add a fullscreen widget
+    const fullscreen = new Fullscreen({
+        view: view
+    });
+    view.ui.add(fullscreen, "top-right");
+
+    // Get the route information from the route layer
+    routeLayer.load().then(function (routeResult) {
+
+        // Get the directions as text
+        var directions = routeLayer.directionPoints.map(d => d.displayText).join('</br></br>');
+        document.getElementById("routeDirections").innerHTML = directions;
+        var lastStop = routeLayer.directionPoints.at(-1);
+        document.getElementById("routeStop").innerHTML = lastStop.name;
+        document.getElementById("directionsSection").style = "display:block";
+
+        // Get all route graphics and add to the map
+        var routeGraphic = routeResult.routeInfo.toGraphic();
+        var startGraphic = routeLayer.stops.items.at(0).toGraphic();
+        var endGraphic = routeLayer.stops.items.at(-1).toGraphic();
+        view.graphics.addMany([routeGraphic, startGraphic, endGraphic]);
+
+        // Change the map extent
+        view.goTo(routeGraphic);
+
+    });
+});
